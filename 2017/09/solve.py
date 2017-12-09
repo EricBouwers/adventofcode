ddacf8d63c17a1ad110822df723055ed25689870
EricBouwers <EricBouwers@gmail.com>
2017-12-09 14:27:17 +0100

    Day 9

---
+++ 2017/09/solve.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys, re, ast
+
+def cleanup(x):
+    x = re.sub(r"\!.","", x)    
+    x = re.sub(r"<[^>]*>", "", x)    
+    return x
+
+def dict_val(x, level_val):
+    result = level_val
+    result += sum(map(lambda y: dict_val(y, level_val+1), x))
+    return result
+
+def score(x):
+    list_rep = x.replace("{","[").replace("}","]")
+    list_rep = re.sub(r"\[,*\]","[]",list_rep)
+    list_rep = re.sub(r",\]","]",list_rep)
+    list_rep = re.sub(r"\[,","[",list_rep)
+    d = ast.literal_eval(list_rep)
+    return dict_val(d, 1) 
+
+def count_garbage(x):
+    x = re.sub(r"\!.","", x)    
+    start = len(x)
+    x = re.sub(r"<[^>]*>", "<>", x)    
+    return start - len(x)
+
+
+if __name__ == '__main__':
+
+    assert cleanup("<>") == ""
+    assert cleanup("<random characters>") == ""
+    assert cleanup("<<<<<>") == ""
+    assert cleanup("<{!>}>") == ""
+    assert cleanup("<!!>") == ""
+    assert cleanup("<!!!>>") == ""
+    assert cleanup("<{o\"i!a,<{i<a>") == ""
+
+    assert score(cleanup("{}")) == 1 
+    assert score(cleanup("{{{}}}")) == 6 
+    assert score(cleanup("{{},{}}")) == 5 
+    assert score(cleanup("{{{},{},{{}}}}")) == 16
+    assert score(cleanup("{<a>,<a>,<a>,<a>}")) == 1 
+    assert score(cleanup("{{<ab>},{<ab>},{<ab>},{<ab>}}")) == 9 
+    assert score(cleanup("{{<!!>},{<!!>},{<!!>},{<!!>}}")) == 9 
+    assert score(cleanup("{{<a!>},{<a!>},{<a!>},{<ab>}}")) == 3 
+
+    assert count_garbage("<>") == 0
+    assert count_garbage("<rancom characters>") == 17
+    assert count_garbage("<<<<>") == 3
+    assert count_garbage("<{!>}>") == 2
+    assert count_garbage("<!!>") == 0
+    assert count_garbage("<!!!>>") == 0
+    assert count_garbage("<{o\"i!a,<{i<a>") == 10
+
+    print score(cleanup(sys.argv[1]))
+    print count_garbage(sys.argv[1])
+
