2b23ddef2d7327fb4873909a684a0dd8c1f4a9c3
ericbouwers <eric@squla.com>
2018-12-06 17:10:39 +0100

    Solving day 6, part 2 with some optimizations

--- 2018/06/solve.py
+++ 2018/06/solve.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, re
+import sys, re, itertools
 
 test_input = """1, 1
 1, 6
@@ -18,7 +18,7 @@ def part1(data):
     squares = {}        
     infinites = set()
 
-    size = max(max([x[0] for x in points]), max([y[1] for y in points])) + 2
+    size = max(max([x[0] for x in points]), max([y[1] for y in points])) + 1
     
     for x in range(0,size):
         for y in range(0,size):
@@ -52,16 +52,32 @@ def part1(data):
 
     return largest_square 
 
-def part2(data):
-    return None
+def part2(data, val):
+    points = [map(int, re.findall('[0-9]+', line)) for line in data.split("\n")]
+    area = 0
+
+    biggest_diff = sum(map(lambda ps: distance(ps[0],ps[1]), [x for x in itertools.permutations(points, 2)]))
+    if biggest_diff > val:
+        min_x = min([x[0] for x in points])
+        min_y = min([y[1] for y in points])
+        max_x = max([x[0] for x in points])
+        max_y = max([y[1] for y in points])
+    else:
+        min_x, min_y, max_x, max_y = (0,0,val,val)
+
+    for x in range(min_x, max_x):
+        for y in range(min_y, max_y):
+            area += 1 if sum(map(lambda p: distance([x,y], p), points)) < val else 0
+
+    return area
 
 if __name__ == '__main__':
 
     assert part1(test_input) == 17
-    assert part2("") == None
+    assert part2(test_input, 32) == 16 
 
     data = sys.argv[1]
 
     print part1(data)
-    print part2(data)
+    print part2(data, 10000)
 
