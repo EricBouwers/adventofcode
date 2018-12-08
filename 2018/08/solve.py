adbad8b8df6fdf9e724e19f9d2643823c1406534
EricBouwers <EricBouwers@gmail.com>
2018-12-08 14:28:26 +0100

    solving day 8 part two, recursion did not work because of class :(

--- 2018/08/solve.py
+++ 2018/08/solve.py
@@ -4,15 +4,30 @@ import sys, re
 
 class Node:
 
-    def __init__(self, children, meta):
-        self.meta = meta
-        self.children = children
+    def __init__(self):
+        self.meta = []
+        self.children = []
 
     def __str__(self):
         m = "meta: " + ",".join(map(str, self.meta))
         c = "children: (" + str(len(self.children)) +  ") " +  ",".join(map(str, self.children))
         return m + " - " + c
 
+    def meta_sum(self):
+        return sum(self.meta) + sum([c.meta_sum() for c in self.children])
+
+    def val_sum(self):
+        value = 0
+        
+        if len(self.children) == 0:
+            value = sum(self.meta)
+        else:
+            child_count = len(self.children)
+            for c in map(lambda x: x - 1, self.meta):
+                if c < child_count:
+                    value += self.children[c].val_sum()
+
+        return value
 
 def parse_nodes(nums):
 
@@ -21,39 +36,46 @@ def parse_nodes(nums):
     childs = nums[0]
     len_meta = nums[1]
     nums = nums[2:]
-    
+    parent = Node()
+
     stack = []
 
     while len(nums) > 0:
         if childs == 0:
-            meta_sum += sum(nums[0:len_meta])    
+            parent.meta = nums[0:len_meta]    
             nums = nums[len_meta:]
             if len(nums) > 0:
-                childs, len_meta = stack.pop()
+                childs, len_meta, old_parent = stack.pop()
+                old_parent.children.append(parent)
+                parent = old_parent
                 
         else:
             childs -= 1
-            stack.append((childs, len_meta))
+            stack.append((childs, len_meta, parent))
 
             childs = nums[0]
             len_meta = nums[1]
             nums = nums[2:]
+            parent = Node()
 
-    return meta_sum
+    return parent
 
 def part1(data):
     nums = map(int, data.split(" "))
-    meta_sum = parse_nodes(nums)
+    tree = parse_nodes(nums)
 
-    return meta_sum
+    return tree.meta_sum()
 
 def part2(data):
-    return None
+    nums = map(int, data.split(" "))
+    tree = parse_nodes(nums)
+
+    return tree.val_sum()
 
 if __name__ == '__main__':
 
     assert part1("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2") == 138
-    assert part2("") == None
+    assert part2("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2") == 66
 
     data = sys.argv[1]
 
