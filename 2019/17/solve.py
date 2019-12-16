85f3a0ac9b472255180e30ae3088b4334435c11d
EricBouwers <EricBouwers@gmail.com>
2019-12-16 17:02:30 +0100

    Prep for tomorrow

---
+++ 2019/17/solve.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+test_1 = """"""
+test_2 = """"""
+test_3 = """"""
+test_4 = """"""
+
+
+def part1(data):
+    return None
+
+
+def part2(data):
+    return None
+
+
+if __name__ == '__main__':
+
+    assert part1(test_1) == None
+    assert part1(test_2) == None
+    assert part2(test_3) == None
+    assert part2(test_4) == None
+
+    with open('input') as f:
+        data = f.read()
+
+    print(part1(data))
+    print(part2(data))
+
