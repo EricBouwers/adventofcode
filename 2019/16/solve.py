945a28e35318afac07f769ee50966f23b1e03749
EricBouwers <EricBouwers@gmail.com>
2019-12-16 16:55:40 +0100

    part 2 with a hint

--- 2019/16/solve.py
+++ 2019/16/solve.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import datetime
+import math
+
 import numpy as np
 from _operator import mul
 
@@ -12,18 +14,14 @@ PHASE_DICT = {}
 
 
 def get_pattern(i, length):
+    global PHASE_DICT
 
     if (i, length) not in PHASE_DICT:
-        p = []
-        pattern_numbers = [0, 1, 0, -1]
-        count_length = length
-        while count_length > -1:
-            pattern_number = pattern_numbers.pop(0)
-            p += i * [pattern_number]
-            pattern_numbers.append(pattern_number)
-            count_length -= i
-
-        PHASE_DICT[(i, length)] = np.array(p[1:length+1])
+        pattern_numbers = [[0], [1], [0], [-1]]
+        steps = sum([i]*4)
+        p = [item for sublist in map(mul, pattern_numbers, [i] * 4) for item in sublist] * (math.ceil(length / steps) + 1)
+        # PHASE_DICT[(i, length)] = np.array(p[1:length + 1])
+        PHASE_DICT[(i, length)] = p[1:length+1]
 
     return PHASE_DICT[(i, length)]
 
@@ -39,12 +37,11 @@ def part1(data, phases):
 
 def run_fft_phases(len_signal, phases, signal):
 
-    signal = np.array(signal)
     while phases > 0:
         new_signal = []
         for i, x in enumerate(signal):
             new_signal.append(
-                abs((signal * get_pattern(i + 1, len_signal)).sum()) % 10)
+                abs(sum(map(mul, signal, get_pattern(i + 1, len_signal)))) % 10)
 
         signal = new_signal
         phases -= 1
@@ -57,9 +54,27 @@ def part2(data):
     len_signal = len(signal)
 
     to_skip = int("".join(map(str, signal[0:7])))
-    signal = run_fft_phases(len_signal, 100, signal)
 
-    return signal[to_skip:to_skip+8]
+    signal = signal[to_skip:]
+    signal.reverse()
+
+    if to_skip > len_signal / 2:
+        phases = 100
+        while phases > 0:
+            new_signal = []
+            prev_x = None
+            for x in signal:
+                new_x = x if not prev_x else (prev_x + x) % 10
+                new_signal.append(new_x)
+                prev_x = new_x
+
+            phases -= 1
+            signal = new_signal
+    else:
+        print("shit")
+
+    signal.reverse()
+    return signal[0:8]
 
 
 if __name__ == '__main__':
@@ -70,8 +85,8 @@ if __name__ == '__main__':
     assert part1(test_2, 100)[0:8] == [2, 4, 1, 7, 6, 1, 7, 6]
     print("done testing part 1")
 
-    assert part2(test_3) == 84462026
-    # assert part2(test_4) == 78725270
+    assert part2(test_3) == [8, 4, 4, 6, 2, 0, 2, 6]
+    assert part2(test_4) == [7, 8, 7, 2, 5, 2, 7, 0]
 
     print("done testing")
 
@@ -81,5 +96,5 @@ if __name__ == '__main__':
     print(datetime.datetime.now())
     print(part1(data, 100)[0:8])
     print(datetime.datetime.now())
-    print(part2(data))
+    print("".join(map(str, part2(data))))
     print(datetime.datetime.now())
