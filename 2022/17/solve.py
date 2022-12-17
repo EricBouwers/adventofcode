05d21e67eb48d14b133b593b2164fb8db9f5fb7b
EricBouwers <EricBouwers@gmail.com>
2022-12-17 23:49:23 +0100

    day 17, part 1

--- 2022/17/solve.py
+++ 2022/17/solve.py
@@ -14,68 +14,71 @@ class keydefaultdict(defaultdict):
             return ret
 
 
-blocks = [
- lambda l, b: ([(l, b), (l+1, b), (l+2, b), (l+3, b)], l, l+3),
- lambda l, b: ([(l+1, b), (l, b+1), (l+1, b+1), (l+2, b+1), (l+1, b+2)], l, l+2),
- lambda l, b: ([(l, b), (l+1, b), (l+2, b), (l+2, b+1), (l+2, b+2)], l, l+2),
- lambda l, b: ([(l, b), (l, b+1), (l, b+2), (l, b+3)], l, l),
- lambda l, b: ([(l, b), (l, b+1), (l+1, b), (l+1, b+1)], l, l+1),
-]
-
-
 def print_cave(max_y, cave):
     for y in range(max_y+3, -1, -1):
         print("".join([str(cave[(x, y)]) for x in range(0,9)]))
 
 
-def part1(data):
+def let_it_fall(data, block_count=2022):
     cave = keydefaultdict(lambda c: 1 if c[0] in [0, 8] or c[1] == 0 else 0)
     jets = [j for j in data[0]]
     max_y = 0
 
-    for i in range(0, 2022):
+    blocks = [
+        lambda l, b: [(l, b), (l + 1, b), (l + 2, b), (l + 3, b)],
+        lambda l, b: [(l + 1, b), (l, b + 1), (l + 1, b + 1), (l + 2, b + 1), (l + 1, b + 2)],
+        lambda l, b: [(l, b), (l + 1, b), (l + 2, b), (l + 2, b + 1), (l + 2, b + 2)],
+        lambda l, b: [(l, b), (l, b + 1), (l, b + 2), (l, b + 3)],
+        lambda l, b: [(l, b), (l, b + 1), (l + 1, b), (l + 1, b + 1)],
+    ]
+
+    i = 0
+    while i < block_count:
         cur_block = blocks.pop(0)
         cur_block_y = max_y + 4
         cur_block_x = 3
 
         cur_block_data = cur_block(cur_block_x, cur_block_y)
 
-        while sum([cave[p] for p in cur_block_data[0]]) == 0:
+        while sum([cave[p] for p in cur_block_data]) == 0:
             jet = jets.pop(0)
-            if jet == '>' and cave[(cur_block_data[2]+1, cur_block_y)] == 0:
+            if jet == '>' and sum([cave[p] for p in cur_block(cur_block_x + 1, cur_block_y)]) == 0:
                 cur_block_x += 1
-                cur_block(cur_block_x, cur_block_y)
-            elif jet == '<' and cave[(cur_block_data[1]-1, cur_block_y)] == 0:
+            elif jet == '<' and sum([cave[p] for p in cur_block(cur_block_x - 1, cur_block_y)]) == 0:
                 cur_block_x -= 1
-                cur_block(cur_block_x, cur_block_y)
 
             jets.append(jet)
 
             cur_block_y -= 1
             cur_block_data = cur_block(cur_block_x, cur_block_y)
 
-        cur_block_data = cur_block(cur_block_x, cur_block_y+1)
-        for p in cur_block_data[0]:
+        cur_block_data = cur_block(cur_block_x, cur_block_y + 1)
+        for p in cur_block_data:
             cave[p] = 1
             max_y = max(max_y, p[1])
         blocks.append(cur_block)
+        i += 1
 
     print(max_y)
     return max_y
 
 
+def part1(data):
+    return let_it_fall(data, 2022)
+
+
 def part2(data):
-    return None
+    return let_it_fall(data, 1000000000000)
 
 
 if __name__ == '__main__':
 
     assert part1(test_1.splitlines()) == 3068
-    assert part2(test_1.splitlines()) == None
+    assert part2(test_1.splitlines()) == 1514285714288
 
     with open('input') as f:
         data = f.read()
 
-    print(part1(data.splitlines()))  # 3101 is too low
+    print(part1(data.splitlines()))  # 3101 is too low, 3148 is too high
     print(part2(data.splitlines()))
 
