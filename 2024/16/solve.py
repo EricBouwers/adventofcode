3204d0bd060d831b487e74c920d3bf74e9df6a4b
EricBouwers <ericbouwers@gmail.com>
2024-12-16 11:53:43 +0100

    day 16, part 2 working but slow

--- 2024/16/solve.py
+++ 2024/16/solve.py
@@ -55,10 +55,10 @@ def parse_data(data):
 
 
 DIRECTION_TO_STEPS = {
-    "N": [('N', -1j, 0), ('E', 1, 1000), ('S', 1j, 2000), ('W', -1, 1000)],
-    "E": [('N', -1j, 1000), ('E', 1, 0), ('S', 1j, 1000), ('W', -1, 2000)],
-    "S": [('N', -1j, 2000), ('E', 1, 1000), ('S', 1j, 0), ('W', -1, 1000)],
-    "W": [('N', -1j, 1000), ('E', 1, 2000), ('S', 1j, 1000), ('W', -1, 0)],
+    "N": [('N', -1j, 1), ('E', 0, 1000), ('W', 0, 1000)],
+    "E": [('N', 0, 1000), ('E', 1, 1), ('S', 0, 1000)],
+    "S": [('E', 0, 1000), ('S', 1j, 1), ('W', 0, 1000)],
+    "W": [('N', 0, 1000), ('S', 0, 1000), ('W', -1, 1)],
 }
 
 
@@ -69,51 +69,79 @@ def get_next_steps(cur_pos, facing, maze):
 
 
 def get_end_paths(maze, start, end):
-    paths = [(start, 'E', 0, set())]
+    paths = [(0, start, 'E', [(start, 'E')])]
     ends = []
     global_seen = defaultdict(lambda: 1000000)
+    all_path = defaultdict(lambda: list())
 
     while paths:
-        cur_pos, facing, score, seen = paths.pop(0)
+        score, cur_pos, facing, seen = paths.pop(0)
         for p, d, s in get_next_steps(cur_pos, facing, maze):
-            new_score = score + s + 1
+            new_score = score + s
             if p == end:
-                ends.append((p, d, new_score, seen | {p}))
-            elif p not in seen and global_seen[(p, d)] > new_score:
-                paths.append((p, d, new_score, seen | {cur_pos}))
+                ends.append((new_score, p, d, seen + [(p, d)]))
+            elif (p, d) not in seen and global_seen[(p, d)] > new_score:
+                paths.append((new_score, p, d, seen + [(p, d)]))
                 global_seen[(p, d)] = new_score
+            elif global_seen[(p, d)] == new_score:
+                all_path[(p, d)].extend(seen)
 
-    return ends
+            paths.sort(key=lambda x: x[0])
+
+    return ends, all_path
 
 
 def part1(data):
     maze, start, end = parse_data(data)
-    return min([e[2] for e in get_end_paths(maze, start, end)])
+    return min([e[0] for e in get_end_paths(maze, start, end)[0]])
+
+
+def print_grid(grid, all_paths, data):
+    for y in range(len(data)):
+        line = ""
+        for x in range(len(data[0])):
+            coor = complex(x, y)
+            line += '*' if (grid[coor] == '#' and coor in all_paths) else "#" if grid[coor] == '#' else 'O' if coor in all_paths else '.'
+
+        print(line)
 
 
 def part2(data):
     maze, start, end = parse_data(data)
     all_pos = set()
-    end_paths = get_end_paths(maze, start, end)
-    best_score = min([e[2] for e in end_paths])
+    end_paths, all_paths = get_end_paths(maze, start, end)
+    best_score = min([e[0] for e in end_paths])
 
-    for end_path in [e for e in end_paths if e[2] == best_score]:
-        all_pos.update(end_path[3])
+    for end_path in [e for e in end_paths if e[0] == best_score]:
+        for p_d in end_path[3]:
+            all_pos.add(p_d)
 
+    updated_all_pos = set()
+    old_len = len(all_pos)
+    new_len = 0
+    while old_len != new_len:
+        old_len = len(all_pos)
+        updated_all_pos.update(all_pos)
+        for pos in all_pos:
+            for path in all_paths[pos]:
+                updated_all_pos.add(path)
+        all_pos = set(updated_all_pos)
+        new_len = len(all_pos)
+
+    print_grid(maze, [x[0] for x in all_pos], data)
     print(len(all_pos), all_pos)
-    return len(all_pos)
+    return len(set([x[0] for x in all_pos]))
 
 
 if __name__ == '__main__':
 
     assert part1(test_1.splitlines()) == 7036
     assert part1(test_2.splitlines()) == 11048
-    # assert part2(test_1.splitlines()) == 45
-    # assert part2(test_1.splitlines()) == 64
+    assert part2(test_1.splitlines()) == 45
+    assert part2(test_2.splitlines()) == 64
 
     with open('input') as f:
         data = f.read()
 
     print(part1(data.splitlines()))
     print(part2(data.splitlines()))
-
